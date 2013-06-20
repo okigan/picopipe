4210447cbdbe2e351e41f352c861e7e50fb01476
Igor Okulist <okigan@yahoo.com>
2013-06-20 14:44:52 -0700

    Add LongProcessingPipeline

--- pycopipe_test.py
+++ pycopipe_test.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 class FutureResult(object):
     def __init__(self, pipeline):
@@ -30,22 +31,23 @@ class Pipeline(object):
         args = list(self.args)
         kwargs = self.kwargs
 
-        #args = [ v.value if isinstance(v, FutureResult) else v for v in args]
-        for idx, fr in enumerate(args):
-            if isinstance(fr, FutureResult):
-               args[idx] = fr.value
+        # Future result must be realized by now, replace them with actual values
+        args = [v.value if isinstance(v, FutureResult) else v for v in args]
 
         g = self.run(*args, **kwargs)
         fr = None
 
         results = []
-        try:
-            while True:
-                result = g.send(fr)
-                fr = FutureResult(result) if isinstance(result, Pipeline) else None
-                results += [result] if fr is None else [fr]  
-        except StopIteration:
-            pass
+        if g is None:
+            results += [None]
+        else:
+            try:
+                while True:
+                    result = g.send(fr)
+                    fr = FutureResult(result) if isinstance(result, Pipeline) else None
+                    results += [result] if fr is None else [fr]  
+            except StopIteration:
+                pass
 
         frs = filter(lambda x: isinstance(x, FutureResult),  results)
         map(FutureResult.materialize, frs)
@@ -80,6 +82,20 @@ class CompositePipeline(Pipeline):
         dummy = yield Max(s, m)
         pass
 
+class SleepPipeline(Pipeline):
+    def run(self, sleep_time):
+        print self, "started sleeping"
+        time.sleep(sleep_time)
+        print self, "done sleeping"
+        pass
+
+class LongProcessingPipeline(Pipeline):
+    def run(self, task_number, processing_time):
+        for i in xrange(task_number):
+            dummy = yield SleepPipeline(processing_time)
+            pass
+        pass
+
 def gen_upper():
     value = yield
     while True:
@@ -110,6 +126,10 @@ def main():
         pass
 
     c = CompositePipeline(1, 2, 3)
+    dummy = c.process()
+    print dummy
+
+    c = LongProcessingPipeline(2, 10)
     dummy = c.process()
     print dummy
 
